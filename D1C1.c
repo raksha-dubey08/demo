210fdbc6cf4d68771f086a8330e8c091f7397168
raksha-dubey08 <rakshadubey380@gmail.com>
2025-09-16 17:30:13 +0530

    Add files via upload

---
+++ D1C1.c
@@ -0,0 +1,26 @@
+/* 
+Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+int main()
+ {
+    int a = 4;  
+    int b = 5;    
+    int sum=0;      
+    sum = a*b; 
+    printf("The sum is: %d", sum);
+    return 0;
+}
