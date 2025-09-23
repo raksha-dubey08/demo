a8b7498ee4abebc685d326521f295bdf87c160e7
raksha-dubey08 <rakshadubey380@gmail.com>
2025-09-23 15:06:39 +0530

    Add files via upload

---
+++ even odd (if - else).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input from user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Check even or odd
+    if (num % 2 == 0) {
+        printf("%d is Even\n", num);
+    } else {
+        printf("%d is Odd\n", num);
+    }
+
+return 0;
+}
+    
