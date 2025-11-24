66107aec8856d358d7e262c47e3736d2e5bc8a95
raksha-dubey08 <rakshadubey380@gmail.com>
2025-11-24 12:57:41 +0530

    Refactor student info program for simplicity
    Removed unnecessary functions and streamlined input/output.

--- studentinfo.c
+++ studentinfo.c
@@ -1,54 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 
-// Define the Student structure
+// Structure definition
 struct Student {
     char name[50];
     int sapID;
     float marks;
 };
 
-// Function to copy student data into the array
-void copyStudent(struct Student *dest, struct Student src) {
-    strcpy(dest->name, src.name);
-    dest->sapID = src.sapID;
-    dest->marks = src.marks;
-}
-
-// Function to display student details
-void displayStudent(struct Student s) {
-    printf("Name: %s\n", s.name);
-    printf("SAP ID: %d\n", s.sapID);
-    printf("Marks: %.2f\n", s.marks);
-    printf("----------------------\n");
-}
-
 int main() {
-    struct Student students[5];
-    struct Student temp;
+    struct Student students[5];  // Array of 5 students
+    int i;
 
-    printf("Enter details for 5 students:\n");
-
-    for (int i = 0; i < 5; i++) {
-        printf("Student %d:\n", i + 1);
+    // Input details of students
+    printf("Enter details of 5 students:\n");
+    for (i = 0; i < 5; i++) {
+        printf("\nStudent %d:\n", i + 1);
         printf("Enter name: ");
-        fgets(temp.name, sizeof(temp.name), stdin);
-        temp.name[strcspn(temp.name, "\n")] = '\0'; // Remove newline
+        scanf("%s", students[i].name);
 
         printf("Enter SAP ID: ");
-        scanf("%d", &temp.sapID);
+        scanf("%d", &students[i].sapID);
 
         printf("Enter marks: ");
-        scanf("%f", &temp.marks);
-        getchar(); // Consume newline left by scanf
-
-        // Copy temp student to array
-        copyStudent(&students[i], temp);
+        scanf("%f", &students[i].marks);
     }
 
-    printf("\nDisplaying student details:\n");
-    for (int i = 0; i < 5; i++) {
-        displayStudent(students[i]);
+    // Display details
+    printf("\n----- Student Details -----\n");
+    for (i = 0; i < 5; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("SAP ID: %d\n", students[i].sapID);
+        printf("Marks: %.2f\n", students[i].marks);
     }
 
     return 0;
